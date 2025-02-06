9ba101f65ae1ec9d8e74a79c88077fc143ecf859
alexis-girard-emf <alexis.girard@studentfr.ch>
2025-02-06 10:33:48 +0100

    modification apiENdpoint

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
-const apiEndpoint =
-  "http://20.107.252.123:8080/api/tasks";
+const apiEndpoint = 
+  "https://backend-210-300621-alexis-girard.blacksky-0acd1103.westeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
